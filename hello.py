ce63f1eb372a454ede87a2fc491861515916bb03
Nafio Miah <nafiomiah@hotmail.com>
2022-09-26 19:51:01 -0400

    Reproduce example 2-2 with hello nafio page

--- hello.py
+++ hello.py
@@ -6,3 +6,8 @@ def simple_hello_world():
     page_title = '<h1>Hello World!</h1>'
     return page_title
 
+@app.route('/helloname/<name>')
+def hello_name(name):
+    page_title = '<h1> Hello, {}! </h1>'.format(name.capitalize())
+    return page_title
+
