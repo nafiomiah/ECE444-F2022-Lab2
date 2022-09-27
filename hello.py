e39cf49e8a0e867509b3f03df8e1e7d5fec56a09
Nafio Miah <nafiomiah@hotmail.com>
2022-09-27 00:53:02 -0400

    Modify hello.py to make use of templates and bootstrap

--- hello.py
+++ hello.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def simple_hello_world():
-    page_title = '<h1>Hello World!</h1>'
-    return page_title
+    # page_title = '<h1>Hello World!</h1>'
+    # return page_title
+    return render_template('index.html')
 
-@app.route('/helloname/<name>')
-def hello_name(name):
-    page_title = '<h1> Hello, {}! </h1>'.format(name.capitalize())
-    return page_title
+@app.route('/helloname/<input_name>')
+def hello_name(input_name):
+    # page_title = '<h1> Hello, {}! </h1>'.format(name.capitalize())
+    # return page_title
+    return render_template('user.html',name = input_name)
+
+
 
