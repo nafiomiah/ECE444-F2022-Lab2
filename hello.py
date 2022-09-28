a0801099b06ee06719e095d02154e2a7cc87952a
Nafio Miah <nafiomiah@hotmail.com>
2022-09-27 21:19:17 -0400

    Reproduce example 4.7 with a name form

--- hello.py
+++ hello.py
@@ -1,31 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, flash
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
+# from flask_moment import Moment
+# from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-moment = Moment(app)
+# moment = Moment(app)
+app.config['SECRET_KEY'] = 'qikgu'
 
-@app.route('/')
-def simple_hello_world():
-    # page_title = '<h1>Hello World!</h1>'
-    # return page_title
-    return render_template('index.html', current_time = datetime.utcnow())
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators = [DataRequired()])
+    submit = SubmitField('Submit')
 
-@app.route('/helloname/<input_name>')
-def hello_name(input_name):
-    # page_title = '<h1> Hello, {}! </h1>'.format(name.capitalize())
-    # return page_title
-    return render_template('user.html',name = input_name)
+@app.route('/', methods=['GET', 'POST'])
+def index_page(): 
+    nf = NameForm()
+    if nf.validate_on_submit():
+        prev_name = session.get('name')
+        if prev_name is not None and prev_name != nf.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = nf.name.data
+        return redirect("/")
+    return render_template('index.html', form = nf, name = session.get('name'))
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+# @app.route('/helloname/<input_name>')
+# def hello_name(input_name):
+#     # page_title = '<h1> Hello, {}! </h1>'.format(name.capitalize())
+#     # return page_title
+#     return render_template('user.html',name = input_name)
 
-@app.errorhandler(500)
-def unhandled_exception(e):
-    return render_template('500.html'), 500
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
+
+# @app.errorhandler(500)
+# def unhandled_exception(e):
+#     return render_template('500.html'), 500
 
 
 
