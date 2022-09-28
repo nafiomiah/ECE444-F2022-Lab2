55ea9e12aa59d5a6f2966221e352b7283a7068d5
Nafio Miah <nafiomiah@hotmail.com>
2022-09-28 01:16:15 -0400

    Add email field for activity 4 part 2

--- hello.py
+++ hello.py
@@ -3,16 +3,19 @@ from flask_bootstrap import Bootstrap
 # from flask_moment import Moment
 # from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 # moment = Moment(app)
-app.config['SECRET_KEY'] = 'qikgu'
+app.config['SECRET_KEY'] = 'abcdef'
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators = [DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators = [DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,8 +26,20 @@ def index_page():
         if prev_name is not None and prev_name != nf.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = nf.name.data
+
+        prev_email = session.get('email')
+        if prev_email is not None and prev_email != nf.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = nf.email.data
+
+        if 'utoronto' in nf.email.data:
+            session['isUofTEmail'] = True
+        elif 'utoronto' not in nf.email.data:
+            session['isUofTEmail'] = False
+
         return redirect("/")
-    return render_template('index.html', form = nf, name = session.get('name'))
+    return render_template('index.html', form = nf, name = session.get('name'), email = session.get('email'), isUofTEmail = session.get('isUofTEmail'))
+
 
 # @app.route('/helloname/<input_name>')
 # def hello_name(input_name):
